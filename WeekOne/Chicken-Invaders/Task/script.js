29f07c93e6d7a37368fe983fa8f3a8f8b863713e
unknown <hazemm7622@gmail.com>
2024-09-05 06:50:40 +0300

    finalize chicken invaders

--- WeekOne/Chicken-Invaders/Task/script.js
+++ WeekOne/Chicken-Invaders/Task/script.js
@@ -2,35 +2,75 @@ var container = document.querySelector(".chickens")
 
 var rocket = document.querySelector('.rocket');
 
+var c=document.querySelector(".container")
+var cont_Size=c.getBoundingClientRect()
+console.log(cont_Size);
+
 for(var i=0 ; i<70 ; i++){
    
     
     var im = document.createElement('img')
     im.src='./Images/Images/Chicken.png'
-
    
    container.append(im); 
 
 }
 
-var positonY = 0; // Correct spelling to 'positionY'
+var positionY = 0; 
 var positionX=0
 window.addEventListener('keydown', function(e) {
   if (e.code === 'ArrowUp') {
-    positonY += 10;
-    rocket.style.bottom = positonY + 'px'; // Update bottom position dynamically
+    positionY += 10;
+    rocket.style.bottom = positionY + 'px'; 
+
+
+    if(positionY>=cont_Size.height-230){
+      positionY=cont_Size.height-230;
+    }
+    
   }else if(e.code === 'ArrowDown'){
-    positonY-=10
-    rocket.style.bottom=positonY +'px';
+    positionY-=10
+    rocket.style.bottom=positionY +'px';
     if(positionY <=10){
-        positonY=10;
+        positionY=10;
     }
   }
   else if(e.code === 'ArrowRight'){
     positionX+=10
     rocket.style.left=positionX +'px';
+
+    if(positionX>= cont_Size.width-100){
+      positionX=cont_Size.width-100;
+    }
+    
   }else if(e.code === 'ArrowLeft'){
     positionX-=10
     rocket.style.left=positionX +'px';
+    if(positionX<=0){
+      positionX=0;
+    }
+  }else if(e.code === 'Space'){
+    var bullet = document.createElement('img')
+
+    bullet.src='./Images/Images/Bullet.png'
+
+    bullet.classList.add('bullet')
+
+    rocket.append(bullet)
+
+    var positionB=0
+    var bulletInterval = setInterval(function(){
+     positionB+=10
+
+      bullet.style.bottom=positionB+'px'
+
+      if(positionB>1000){
+        clearInterval(bulletInterval)
+        rocket.removeChild(bullet)
+
+      }
+
+    } , 10 )
+
   }
 });
